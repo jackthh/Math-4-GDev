f497dab4c1b3e4681dbf9aacbfc7a962b368935c
FLORA\Jack <jack.truonghoanghiep@gmail.com>
2023-07-14 01:00:25 +0700

    Asgmt 1

---
+++ Assets/_Math 4 GDev/Scripts/Part1Asgmt1.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+
+namespace _Math_4_GDev.Scripts
+{
+    public class Part1Asgmt1 : MonoBehaviour
+    {
+        public Transform checkingTarget;
+        public float checkingRadius;
+
+
+        private void OnDrawGizmos()
+        {
+            var transformPosition = transform.position;
+            var sqrtDistToTarget = Vector3.SqrMagnitude(checkingTarget.position - transformPosition);
+
+            Handles.color = sqrtDistToTarget >= checkingRadius * checkingRadius ? Color.cyan : Color.red;
+
+
+            Handles.DrawWireArc(transformPosition, Vector3.forward, Vector3.right, 360, checkingRadius);
+        }
+    }
+}
