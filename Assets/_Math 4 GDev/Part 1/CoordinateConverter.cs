4879c8d4d7a83327a9382c2a524000d88aa14674
FLORA\Jack <jack.truonghoanghiep@gmail.com>
2023-07-15 15:28:24 +0700

    Converted local to world

--- Assets/_Math 4 GDev/Part 1/CoordinateConverter.cs
+++ Assets/_Math 4 GDev/Part 1/CoordinateConverter.cs
@@ -12,27 +12,43 @@ namespace _Math_4_GDev.Part_1
         public Vector2 originalLocalPoint;
 
 
-        [Button(ButtonSizes.Large)] public void WorldToLocal()
+        private Vector2 GetWorldToLocal(Vector2 _worldPos)
         {
-            var worldOffset = originalWorldPoint - new Vector2(localCoordinate.position.x, localCoordinate.position.y);
+            var worldOffset = _worldPos - new Vector2(localCoordinate.position.x, localCoordinate.position.y);
             var localX = Vector3.Dot(localCoordinate.right, worldOffset);
             var localY = Vector3.Dot(localCoordinate.up, worldOffset);
 
-            Debug.Log($"Local Pos: {new Vector2(localX, localY)}");
+            return new Vector2(localX, localY);
+        }
+
+
+        [Button(ButtonSizes.Large)] public void WorldToLocal()
+        {
+            Debug.Log($"Local Pos: {GetWorldToLocal(originalWorldPoint)}");
         }
 
 
         [Button(ButtonSizes.Large)] public void LocalToWorld()
         {
-            //  NOTE: To convert root point in world space in to local space 
-            var worldOffset = -localCoordinate.position;
-            var localX = Vector3.Dot(localCoordinate.right, worldOffset);
-            var localY = Vector3.Dot(localCoordinate.up, worldOffset);
+            // //  NOTE: To convert root point in world space in to local space 
+            // var worldOffset = -localCoordinate.position;
+            // var localX = Vector3.Dot(localCoordinate.right, worldOffset);
+            // var localY = Vector3.Dot(localCoordinate.up, worldOffset);
+            //
+            // var worldRootInLocal = new Vector2(localX, localY);
+            // var originalLocalPointInWorld = originalLocalPoint - worldRootInLocal;
+            //
+            // Debug.Log($"World Pos: {new Vector2(originalLocalPointInWorld.x, originalLocalPointInWorld.y)}");
 
-            var worldRootInLocal = new Vector2(localX, localY);
-            var originalLocalPointInWorld = originalLocalPoint - worldRootInLocal;
 
-            Debug.Log($"World Pos: {new Vector2(originalLocalPointInWorld.x, originalLocalPointInWorld.y)}");
+            //  NOTE:   Local pos in world coordinate axis but still local root
+            var worldXSameRoot = Vector3.Dot(GetWorldToLocal(localCoordinate.position + Vector3.right), originalLocalPoint);
+            var worldYSameRoot = Vector3.Dot(GetWorldToLocal(localCoordinate.position + Vector3.up), originalLocalPoint);
+
+            //  NOTE:   Add local coordinate root position in world space
+            var result = new Vector2(worldXSameRoot + localCoordinate.position.x, worldYSameRoot + localCoordinate.position.y);
+
+            Debug.Log($"World Pos: {result}");
         }
     }
 }
